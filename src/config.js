b96ebc67af1ca62d2a17efeb8c5b898861578f4b
Lux Lu <luxlu0172@gmail.com>
2024-06-10 19:07:10 +0800

    init

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
 const CONFIG = {
     'ga': 'G-6C4Z9NHW8J',
-    'whiteboard_id': 'd4cc3728297609add1a00aab108e90c4e57a1c378cfc2307c251745bf7d2a884',
-    'title': '数字花园🌱', // 站点标题
+    'whiteboard_id': 'c29db69b3115f559e248cf84af2558326c63140c8622501bd767f47738a815b4',
+    'title': "🍃 Lux's Digital Garden 🌱", // 站点标题
     'pages': {
-        'Articles': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
-        'Projects': '3dd9a603-a7f3-44e9-a6d7-cd2ebda08952',
+        'About Me': '7cd752cf-ac3c-44af-b642-5318fc57a161',
+        'Projects': 'e68faa5b-a9ce-42c9-b173-a398c1712864',
+        'Writing': 'c2644491-25e6-4a60-a102-e6939416a111',
+        'Reading': '31e97469-0532-4437-b892-ee5e754e001d',
+        'Hobbies': 'f612f2d5-5b10-48b3-803c-a96d3d4beda5'
         // 'Activity':'activity', // 花园活跃状态页面
-        'About': '3a433c0b-e2e1-4722-8a88-a17e9aa2b927'
     },
     'server': '1214087861653995540', // Discord 服务器 ID，非必填，填写后将在网站中显示聊天入口
     'channel': '1214087861653995545' // Discord 频道 ID，非必填，填写后将在网站中显示聊天入口
